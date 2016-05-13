ed5bdfdb5a4f996ddc1a98ba104a2f5596e72716
g13916kh <g13916kh@s30450.tsuda.ac.jp>
2016-05-13 11:50:16 +0900

    数値変換

--- src/junit/inttoeng/IntToEng.java
+++ src/junit/inttoeng/IntToEng.java
@@ -6,7 +6,7 @@ public class IntToEng {
 
     // メインメソッド
     public static void main(String[] args) {
-
+ 	
         Scanner sc = new Scanner(System.in);
         int input = sc.nextInt();
 
@@ -16,7 +16,30 @@ public class IntToEng {
 
     // 数値を英訳する変換するメソッド
     static String translateEng(int n) {
-    	
-        return "";
+    	String number = "";
+    		//public static String toEnglishNumber(String input) throws NumberFormatException {
+    		final String[] eNum1 = {"zero", "one", "two", "three", "four",
+    		"five", "six", "seven", "eight", "nine",
+    		"ten", "eleven", "twelve", "thirteen", "fourteen",
+    		"fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+    		 final String[] eNum2 = {"twenty", "thirty", "forty", "fifty", "sixty",
+    		    		"seventy", "eighty", "ninety"};
+    		if (n < 0 || n > 99) {
+    		throw new NumberFormatException();
+    		}
+    		if (n < 20) {
+    	    number = eNum1[n];
+    		} else if(n >= 20 && n % 10 == 0) {
+    		number = eNum2[n/10-2];
+    		}
+    		/*number = new StringBuffer(eNum2[input.charAt(0) - 50]);
+    		if (input.charAt(1) != '0') {
+    		number.append("-").append(eNum1[input.charAt(1) - 48]);
+    		}
+    		}
+    		return number.toString();
+    		}
+    		}*/
+        return number;
     }
 }
--- test/junit/inttoeng/IntToEngTest.java
+++ test/junit/inttoeng/IntToEngTest.java
@@ -5,15 +5,19 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import junit.tutorial.Calculator;
-
 public class IntToEngTest {
 
 	@Test
 	public void translateで1をoneに変更できるメソッド() {
 		IntToEng number = new IntToEng();
 		String expected = "one";
-		int actual = number.translateEng(1);
+		String actual = number.translateEng(1);
+		assertThat(actual,is(expected));
+	}
+	public void translateで30をthirtyに変更できるメソッド() {
+		IntToEng number = new IntToEng();
+		String expected = "thirty";
+		String actual = number.translateEng(30);
 		assertThat(actual,is(expected));
 	}
 	public void test() {
